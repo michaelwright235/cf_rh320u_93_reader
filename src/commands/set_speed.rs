89804b160f320f5ea2f32a4a6cbc98eb9a73620c
michaelwright235 <maiklpolkovnikov@gmail.com>
2022-08-18 13:28:10 +0300

    Optimized set_speed func

--- src/commands/set_speed.rs
+++ src/commands/set_speed.rs
@@ -1,30 +1,20 @@
 use crate::*;
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[repr(u8)]
 pub enum Speed {
-    S9600,
-    S19200,
-    S38400,
-    S57600,
-    S115200
+    S9600 = 0x00,
+    S19200 = 0x01,
+    S38400 = 0x02,
+    S57600 = 0x03,
+    S115200 = 0x04
 }
 pub fn set_speed(speed: Speed) ->  Result<(), ReaderError> {
     let device = CFRH320U93::init()?;
     let mut buffer = Buffer::new();
     buffer.write(0x02);
     buffer.write(0x81);
-
-    let speed_byte:u8;
-
-    match speed {
-        Speed::S9600 => speed_byte = 0x00,
-        Speed::S19200 => speed_byte = 0x01,
-        Speed::S38400 => speed_byte = 0x02,
-        Speed::S57600 => speed_byte = 0x03,
-        Speed::S115200 => speed_byte = 0x04,
-    }
-
-    buffer.write(speed_byte);
+    buffer.write(speed as u8);
 
     device.set_report(buffer.get())?;
 
