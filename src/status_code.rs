bcb5025638aa55cf1b4c1db59e68d0f9ff480033
michaelwright235 <maiklpolkovnikov@gmail.com>
2023-11-13 01:41:14 +0300

    Use `thiserror` crate for error handling

--- src/status_code.rs
+++ src/status_code.rs
@@ -1,95 +1,80 @@
-use std::{error::Error, fmt::Display};
+use thiserror::Error;
 
-/// ReaderError enum contains two types of possible errors:
-/// UsbError and CommandError.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+/// There're two types of possible errors:
+/// `UsbError` and `CommandError`. If the command is not implemented, a
+/// `NotImplemented` variant is returned.
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Error)]
 pub enum ReaderError {
     /// UsbError is returned if there was some kind of connection problem.
-    UsbError(rusb::Error),
+    #[error("{0}")]
+    UsbError(#[from] rusb::Error),
+
     /// CommandError is returned if a reader couldn't proccess given command in some way.
-    CommandError(StatusCode),
+    #[error("{0}")]
+    CommandError(#[from] StatusCode),
+
     /// This command is not implemented yet.
+    #[error("Not implemented yet")]
     NotImplemented,
 }
 
-impl Display for ReaderError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ReaderError::UsbError(e) => f.write_str(e.to_string().as_str()),
-            ReaderError::CommandError(e) => f.write_str(e.to_string().as_str()),
-            ReaderError::NotImplemented => f.write_str("Not implemented yet"),
-        }
-    }
-}
-
-impl From<rusb::Error> for ReaderError {
-    fn from(e: rusb::Error) -> Self {
-        Self::UsbError(e)
-    }
-}
-
-impl From<StatusCode> for ReaderError {
-    fn from(e: StatusCode) -> Self {
-        Self::CommandError(e)
-    }
-}
-
-impl Error for ReaderError {}
-
 /// Status codes that a reader can throw.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Error)]
 pub enum StatusCode {
+    #[error("Success")]
     Ok,
+
+    #[error("Failure")]
     Failure,
+
+    #[error("Timeout error")]
     TimeoutError,
+
+    #[error("Card doesn't exist")]
     NoCard,
+
+    #[error("Receiving card data error")]
     InvalidData,
+
+    #[error("The input parameter or input command format is incorrect")]
     AuthenticationFailure,
+
+    #[error("Internal error")]
     InternalError,
+
+    #[error("Unknown error")]
     UnknownError,
+
+    #[error("Operation error")]
     OperationError,
+
+    #[error("Unknown command")]
     UnknownCommand,
+
+    #[error("Unsupported command for this type of card")]
     UnsupportedCommandForCard,
+
+    #[error("Invalid command format")]
     InvalidCommandFormat,
+
+    #[error("In the FLAG parameter of the command, OPTION mode is not supported (only for 15693 commands)")]
     UnsupportedOptionForm, // ?
+
+    #[error("Specified block doesn't exit")]
     BlockDoesntExist,
+
+    #[error("Specified block is locked")]
     BlockIsLocked,
+
+    #[error("Couldn't lock specified block")]
     LockBlockFailure,
+
+    #[error("Failed to write data")]
     WriteFailure,
+
+    #[error("Received unknown status code: {0}")]
     UnknownCode(u8),
 }
-
-impl Display for StatusCode {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if let StatusCode::UnknownCode(c) = self {
-            let un = String::from("Received unknown status code: ") + format!("{:#X}", c).as_str();
-            return f.write_str(un.as_str());
-        }
-
-        f.write_str(match self {
-            StatusCode::Ok => "Success",
-            StatusCode::Failure => "Failure",
-            StatusCode::TimeoutError => "Timeout error",
-            StatusCode::NoCard => "Card doesn't exist",
-            StatusCode::InvalidData => "Receiving card data error",
-            StatusCode::AuthenticationFailure => "The input parameter or input command format is incorrect",
-            StatusCode::InternalError => "Internal error",
-            StatusCode::UnknownError => "Unknown error",
-            StatusCode::OperationError => "Operation error",
-            StatusCode::UnknownCommand => "Unknown command",
-            StatusCode::UnsupportedCommandForCard => "Unsupported command for this type of card",
-            StatusCode::InvalidCommandFormat => "Invalid command format",
-            StatusCode::UnsupportedOptionForm => "In the FLAG parameter of the command, OPTION mode is not supported (only for 15693 commands)", //?
-            StatusCode::BlockDoesntExist => "Specified block doesn't exit",
-            StatusCode::BlockIsLocked => "Specified block is locked",
-            StatusCode::LockBlockFailure => "Couldn't lock specified block",
-            StatusCode::WriteFailure => "Failed to write data",
-            _ => "",
-        })
-    }
-}
-
-impl Error for StatusCode {}
 
 impl From<u8> for StatusCode {
     fn from(byte: u8) -> Self {
